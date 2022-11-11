d6c7be69887229038a6e0839363494b407b4e0ee
Muhammad Ilham Kurniawan <muh.ilhamkurniawan11@gmail.com>
2022-11-11 11:23:38 +0700

    perubahan nama css

--- operator.php
+++ operator.php
@@ -9,7 +9,7 @@ error_reporting(0);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="setting3.css">
+    <link rel="stylesheet" type="text/css" href="operator.css">
     <title>Layout Operator</title>
 </head>
 <body>
--- view.php
+++ view.php
@@ -19,7 +19,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="setting.css">
+    <link rel="stylesheet" type="text/css" href="view.css">
     <title>Layout Peron</title>
 </head>
 <body>
