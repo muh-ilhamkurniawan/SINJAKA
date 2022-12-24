dc02a9d59ad958f4af1b6e127d3edc3b1a8a49b0
muh-ilhamkurniawan <93339557+muh-ilhamkurniawan@users.noreply.github.com>
2022-12-24 22:03:31 +0700

    Tambah search

--- activity.php
+++ activity.php
@@ -10,6 +10,16 @@ ini_set("display_errors",0);
 error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED);
 error_reporting(0);
 ?>
+<?php
+        if(isset($_GET["cari"])){
+            $daftardata = $_GET["cari"];
+            $search = "SELECT * FROM activity_log
+                WHERE  nama_user LIKE '%".$daftardata."%' OR
+                        peran LIKE '%".$daftardata."%'  ORDER BY no desc";				
+                }else{
+                    $search = "SELECT * FROM activity_log ORDER BY no desc";	
+        }
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -34,6 +44,35 @@ error_reporting(0);
                 <div class="listakun"> 
                     Log Aktivitas Akun
                 </div>
+                <div class="search">
+                    <form action="activity.php" method="get">
+                            <input type="text" name="cari" id="search" for="search" size="40"  autofocus>
+                            <button class="tombol-link" type="submit" name="caridata" autocomplete="off" id="search">Search</button>
+                    </form>
+                    <!-- <form method='post' name="loginform"  action='<?php $_SERVER['PHP_SELF']; 
+                     ?>'>
+                    <input type="checkbox" onclick="myFunction()" >Kutoarjo
+							<script>
+								function myFunction() {
+                                    <?php
+                                    // $search = "SELECT * FROM activity_log WHERE peran='OP Purwokerto' ";
+                                    ?>
+								}
+							</script>
+                    </form> -->
+                    <!-- <form method='post' name="loginform"  action='<?php $_SERVER['PHP_SELF']; 
+                     ?>'>
+                    <input type="checkbox" onclick="myFunction()" >Purwokerto
+							<script>
+								function myFunction() {
+                                    <?php
+                                    // $search = "SELECT * FROM activity_log WHERE peran='OP Purwokerto' ";
+                                    ?>
+								}
+							</script>
+                    </form> -->
+                </div>
+
                 <div id="boxdalam">
                     <table class="table-logger">
                         <tr>
@@ -46,8 +85,8 @@ error_reporting(0);
                         </tr>
                         <?php
                             $no = 1;
-                            $sql = "select * from activity_log order by no desc";
-                            $query = mysqli_query($conn,$sql);
+                            // $sql = "select * from activity_log order by no desc";
+                            $query = mysqli_query($conn,$search);
                             while($row = mysqli_fetch_array($query)) :
                         ?>
                             <tr>
